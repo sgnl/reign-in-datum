bd6b8497a37920bf8041d9c66fdd97d2ea95395b
sgnl <rayrfarias@gmail.com>
2017-01-27 13:34:29 -1000

    updates with comments

--- reign-in-datum.js
+++ reign-in-datum.js
@@ -1,4 +1,21 @@
 
+/**
+ * Helper wrapping function
+ * prepares data for easier use
+ *
+ * @param Function stores function away to be called at a later time
+ * @return Function this is the 'wrapper'
+ */
+const wrapper = (fn) => (city, data) => fn(city, pluckCityData(city, data));
+
+/**
+ * Gives back all values found for a city within data
+ *
+ * @param  String   city         e.g. "London", "Beijing", ...
+ * @param  String   year         unused, _ is a placeholder
+ * @param  String   collection   long list of data, gross!
+ * @return Array    an array of Number values
+ */
 const pluckCityData = (city, [_, [ collection ] ] = data) => {
   return collection.split('\n').filter(cityData => {
     return cityData.split(':')[0] === city;
@@ -8,12 +25,24 @@ const pluckCityData = (city, [_, [ collection ] ] = data) => {
   .map(month => parseFloat(month.split(' ')[1]));
 };
 
-const wrapper = (fn) => (city, data) => fn(city, pluckCityData(city, data));
-
+/**
+ * returns the mean temperatures for a given city
+ *
+ * @param  String   city  e.g. "London", "Beijing", ...
+ * @param  Array    data  e.g. [1, 2, 3, 4, ...]
+ * @return Number   mean  temperature value
+ */
 const mean = (city, data) => {
   return data.reduce((store, val) => store + val, 0) / data.length;
 };
 
+/**
+ * returns the variance of temperatures for a given city
+ *
+ * @param  String   city  e.g. "London", "Beijing", ...
+ * @param  Array    data  e.g. [1, 2, 3, 4, ...]
+ * @return Number   mean  temperature value
+ */
 const variance = (city, data) => {
   const cityMean = mean(city, data);
   const variance = data.map(val => (val - cityMean) * (val - cityMean));
