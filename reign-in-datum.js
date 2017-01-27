7fc41fd0299480083ad0e4d82312d53e8c618e64
sgnl <rayrfarias@gmail.com>
2017-01-27 12:45:05 -1000

    implements pluckCityData helper

--- reign-in-datum.js
+++ reign-in-datum.js
@@ -1,4 +1,13 @@
 
+const pluckCityData = (city, [_, [ collection ] ] = data) => {
+  return collection.split('\n').filter(cityData => {
+    return cityData.split(':')[0] === city;
+  })
+  .map(city => city.split(':')[1])[0]
+  .split(',')
+  .map(month => parseFloat(month.split(' ')[1]));
+};
+
 const mean = (city, data) => {
 
 };
@@ -7,4 +16,4 @@ const variance = (city, data) => {
 
 };
 
-module.exports = { mean, variance };
+module.exports = { mean, variance, pluckCityData };
